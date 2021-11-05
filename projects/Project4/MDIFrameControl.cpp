9b43402b42db8832c1041f5c5c839f5644504bf9
furuya <sp_furuya@hotmail.com>
2021-11-05 18:03:35 +0900

    1st

--- projects/Project4/MDIFrameControl.cpp
+++ projects/Project4/MDIFrameControl.cpp
@@ -117,7 +117,7 @@ void CreateMDISplitControl(HWND hWnd)
 	
 	//D2DCreateSquarePaper(hwin,ha, FRectF(140,140, FSizeF(300,300)),  STAT_DEFAULT, L"DEBUG1", 192);
 
-	D2DCreateWhiteWindow(hwin,ha, FRectF(250,250, FSizeF(900,900)),  STAT_DEFAULT, L"AAAAA", 192);
+	//D2DCreateWhiteWindow(hwin,ha, FRectF(250,250, FSizeF(900,900)),  STAT_DEFAULT, L"AAAAA", 192);
 	
 
 }
